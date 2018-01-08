609ec52784b66ea07ab8638272b48dc0ab5a58bb
Ariel Rodriguez <ariel@volonbolon.me>
2018-01-08 12:26:34 -0300

    Initial Commit

---
+++ weather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  weather
+//
+//  Created by Ariel Rodriguez on 08/01/2018.
+//  Copyright © 2018 Ariel Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
